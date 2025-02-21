082250c0d9435597610a4a7d6b8539ebce26b80d
kaiqued1 <kaiqued1@insper.edu.br>
2025-02-21 13:49:25 +0000

    SAasA

--- testing_bluepill/main.c
+++ testing_bluepill/main.c
@@ -1,33 +1,28 @@
 #include "RTE_Components.h"
 #include CMSIS_device_header
 
-void wait(){
-	int d;
-	for (d=0; d < 1500000; d++);
+void wait() {
+  int d;
+  for (d = 0; d < 500000; d++);
 }
 
+int main() {
 
+  RCC->APB2ENR |= (1UL << 2);
 
-int main (){
-	
-	RCC->APB2ENR |= (1UL << 2);
-	
-	GPIOA->CRL  = 0x33333333;
-	
-	GPIOA->CRH  = 0x33333333;
-	
-	while(1){
-		for (int i = 0; i < 8; i++){
-			GPIOA->BSRR = 1UL << i;
-			wait();
-			GPIOA->BRR = 1UL << i;
-		}
+  GPIOA->CRL = 0x33333333;
 
-		for (int i = 7; i >= 0; i--){
-			GPIOA->BSRR = 1UL << i;
-			wait();
-			GPIOA->BRR = 1UL << i;
-		}
-	}
-	
+  GPIOA->CRH = 0x33333333;
+
+  while (1) {
+    for (int i = 0; i < 8; i++) {
+      GPIOA->BSRR = 1UL << i;
+      wait();
+    }
+
+    for (int i = 7; i >= 0; i--) {
+      GPIOA->BRR = 1UL << i;
+      wait();
+    }
+  }
 }
